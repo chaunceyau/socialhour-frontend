1795d8b9cedf241fe42dd5c07b612999c2d8a5c4
Austin <chauncey.au@gmail.com>
2019-07-29 11:20:21 -0500

    REACT_APP_BACKEND_URL

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'semantic-ui-css/semantic.min.css'
 
 export const client = new ApolloClient({
   // uri: process.env.NODE_ENV === 'production' ? 'http://backend.socialhour.tv/graphql' : 'http://localhost:4000/graphql',
-  uri: process.env.BACKEND_URL,
+  uri: process.env.REACT_APP_BACKEND_URL,
   request: async operation => {
     let token = null
     console.log(process.env.BACKEND_URL, process.env.NODE_ENV, "fldsam")
